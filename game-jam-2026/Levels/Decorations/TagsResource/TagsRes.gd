6528f093013354fc6e60b2fef45dbf355b669d7d
Gianfranco Baccarella <90854992+RedFoXalabama@users.noreply.github.com>
2026-02-01 13:18:19 +0100

    Add end level UI and scoring logic to main level
    Introduces an end-of-level screen with an exit button and updates the LevelController and CharacterInterface scripts to support scoring and result calculation based on decorations and tags. Updates scene files to support new mask decorations, expand modes, and result display. Also adds the ExitLevelButton script and integrates it into the end layer.

--- game-jam-2026/Levels/Decorations/TagsResource/TagsRes.gd
+++ game-jam-2026/Levels/Decorations/TagsResource/TagsRes.gd
@@ -3,7 +3,7 @@ class_name TagsRes
 
 @export var name : String
 
-@export_enum("Up","Center","Down") var position : int
+@export_enum("Up","Center","Down", "Mask") var position : int
 @export_enum("Horror","Cute","Elegant") var theme : int
 @export var colors : Array[ColorTag]
 @export var subthemes : Array[SubThemeTag]
--- game-jam-2026/Levels/LevelController.gd
+++ game-jam-2026/Levels/LevelController.gd
@@ -2,13 +2,13 @@ extends Control
 class_name LevelController
 
 @onready var game_interface : SplitContainer = $GameInterface
-#@onready var char_inter : CharacterInterface = $GameInterface/CharacterInterface
 @onready var query_text : RichTextLabel = $GameInterface/QueryPanel/QueryText
 @onready var timer : Timer = $GameInterface/VSplitContainer/Time_PauseContainer/TimerPanel/Timer
 @onready var timer_label : Label = $GameInterface/VSplitContainer/Time_PauseContainer/TimerPanel/Label
 @onready var pause_layer : CanvasLayer = $PauseLayer
 @onready var result_layer : CanvasLayer = $ResultLayer
 @onready var gallery_layer : CanvasLayer = $GalleryLayer
+@onready var end_layer : CanvasLayer = $EndLayer
 var scene_name_to_save : String
 
 @export var characters_to_serve : Array[CharacterInterface]
@@ -21,20 +21,25 @@ func _process(delta: float) -> void:
 	timer_label.text = str(int(timer.time_left))
 
 func start_turn():
-	var turn_char = characters_to_serve[turn_idx]
-	game_interface.split_offsets = [-100, 100]
-	get_node("GameInterface/"+ turn_char.name).show()
-	query_text.text = turn_char.query
-	turn_char.show()
-	timer.start()
+	if (turn_idx + 1 <= len(characters_to_serve)):
+		var turn_char = characters_to_serve[turn_idx]
+		game_interface.split_offsets = [-100, 100]
+		get_node("GameInterface/"+ turn_char.name).show()
+		query_text.text = turn_char.query
+		game_interface.show()
+		turn_char.show()
+		timer.start()
+	else:
+		end_level()
+	
 
 func end_turn():
 	var scene = PackedScene.new()
 	scene.pack(characters_to_serve[turn_idx])
 	var scene_name = characters_to_serve[turn_idx].name
 	ResourceSaver.save(scene, "res://TempFrames/"+ scene_name +".tscn")
-	await get_tree().create_timer(3).timeout
-	#CONTO DEI RISULTATI
+	await get_tree().create_timer(2).timeout
+	#POSIZIONAMENTO DEL RISULTATO CREATO
 	game_interface.hide()
 	result_layer.show()
 	result_layer.get_node("Control/Frame/ImageLimit").add_child(scene.instantiate())
@@ -44,20 +49,30 @@ func end_turn():
 	new_frame.set_size(image_limit.size)
 	new_frame.set_position(image_limit.position)
 	new_frame.char_rect.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
+	#CALCOLO DEL RISULTATO
+	var results = characters_to_serve[turn_idx].final_results()
+	var results_label :Label = result_layer.get_node("Control/Label")
+	results_label.text = results["response"] + "\nPunteggio finale: " + str(results["points"])
 	
 func show_gallery():
 	result_layer.hide()
 	gallery_layer.show()
 
+func end_level():
+	game_interface.hide()
+	end_layer.show()
+	
 func _decoration_button_pressed(deco_res : DecoRes):
 	var turn_char = characters_to_serve[turn_idx]
 	match deco_res.tags.position:
 		0:
-			turn_char.add_up(deco_res.image)
+			turn_char.add_up(deco_res)
 		1:
-			turn_char.add_center(deco_res.image)
+			turn_char.add_center(deco_res)
 		2:
-			turn_char.add_down(deco_res.image)
+			turn_char.add_down(deco_res)
+		3:
+			turn_char.add_mask(deco_res)
 
 func _on_timer_timeout() -> void:
 	end_turn()
@@ -127,6 +142,5 @@ func _on_exit_gallery_button_pressed() -> void:
 	gallery_layer.hide()
 	var turn_char = characters_to_serve[turn_idx]
 	get_node("GameInterface/"+ turn_char.name).hide()
-	game_interface.show()
 	turn_idx += 1
 	start_turn()
